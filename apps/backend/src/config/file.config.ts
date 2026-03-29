b89b096e40c007740be9d2c36972aba99e182796
Dinesh Tomar <dinesh.tomar@dapt.tech>
2026-03-29 21:52:56 +0530

    fix(storage): read custom s3 endpoint config

--- apps/backend/src/config/file.config.ts
+++ apps/backend/src/config/file.config.ts
@@ -6,6 +6,8 @@ export type FileConfig = {
   secretAccessKey?: string;
   awsDefaultS3Bucket?: string;
   awsS3Region?: string;
+  awsS3Endpoint?: string;
+  awsS3ForcePathStyle: boolean;
   maxFileSize: number;
 };
 
@@ -15,5 +17,7 @@ export default registerAs<FileConfig>('file', () => ({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   awsDefaultS3Bucket: process.env.AWS_DEFAULT_S3_BUCKET,
   awsS3Region: process.env.AWS_S3_REGION,
+  awsS3Endpoint: process.env.AWS_S3_ENDPOINT,
+  awsS3ForcePathStyle: process.env.AWS_S3_FORCE_PATH_STYLE === 'true',
   maxFileSize: 5242880, // 5mb
 }));
